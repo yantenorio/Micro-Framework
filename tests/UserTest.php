7ba84b4ff0243a97a7636215dc4a26e91204223c
yan <Yantenorio64@gmail.com>
2022-02-01 09:35:49 -0300

    testing phpunit

---
+++ tests/UserTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testUserShouldCreateAccount()
+    {
+        $this->assertEquals(1,1);
+    }
+}
