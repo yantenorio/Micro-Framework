67d21964457e0356eb0c804f3ea57ac8df395165
yan <Yantenorio64@gmail.com>
2022-02-01 09:50:23 -0300

    test UserShouldCreateAccount

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -1,11 +1,22 @@
 <?php
 
+use App\Models\User;
 use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
     public function testUserShouldCreateAccount()
     {
-        $this->assertEquals(1,1);
+        $user = new User();
+
+        $payload = [
+            "name" => "user",
+            "email" => "test@email.com",
+            "password" => "secret123"
+        ];
+
+        $result = $user->postUser($payload);
+
+        $this->assertTrue($result);
     }
 }
